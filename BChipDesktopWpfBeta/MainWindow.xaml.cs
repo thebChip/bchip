d9f7b9bf8271cd01ae760388eebfbbd580aa2c5a
saporis <daniel@simpletofind.ca>
2018-06-19 00:16:00 -0700

    USB device monitoring, including card insert and removal.

--- BChipDesktopWpfBeta/MainWindow.xaml.cs
+++ BChipDesktopWpfBeta/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -29,14 +30,11 @@ namespace BChipDesktop
     public partial class MainWindow : Window
     {
         public StreamWriter logWriter = null;
-        // TODO: Eventually clear the passphrase from memory or 
-        //       re-write this to be a tad more secure.
-        private System.Threading.Timer passphraseClearTimer = null;
-
         private static readonly IContextFactory _contextFactory = ContextFactory.Instance;
         private ISCardMonitor _monitor = MonitorFactory.Instance.Create(SCardScope.System);
 
-        private ConcurrentBag<BChipSmartCard> LoadedBChips = new ConcurrentBag<BChipSmartCard>();
+        // Only support a single bchip, regardless of reader(s)
+        private BChipSmartCard LoadedBChips = null;
 
         public MainWindow()
         {
@@ -47,59 +45,25 @@ namespace BChipDesktop
                 {
                     WriteToLogFile($"App started", "Startup");
 
-                    _monitor.CardInserted += _monitor_CardInserted;
-                    //(sender, args) =>
-                    //MessageBox.Show($"New state: {args.State}");
-                    _monitor.CardRemoved += _monitor_CardRemoved;
                     StartMonitoring();
 
                     //WriteToLogFile($"Load cards on disk", "Startup");
 
                     InitializeComponent();
-                    //SetupDataGridView();
-                    //HideAllPassphraseUI();
-                    //sendBtn.Enabled = false;
-                    //ReceiveBtn.Enabled = false;
-                    //notConnectedIcon.Visible = true;
-
+                    
                     // Check for plugged in cards
                     ScanAndLoadConnectedCards();
 
-                    WriteToLogFile($"Timer started", "Startup");
-                    passphraseClearTimer = new System.Threading.Timer(
-                     _ =>
-                     {
-                         string statusMessage = String.Empty;
-                         //if (!String.IsNullOrWhiteSpace(privateKey))
-                         //{
-                         //    if (DateTime.Now.Subtract(pkSetTime) > TimeSpan.FromSeconds(30))
-                         //    {
-                         //        privateKey = String.Empty;
-                         //    }
-                         //    else
-                         //    {
-                         //        statusMessage += $"Pkey cleared in {DateTime.Now.Subtract(pkSetTime).TotalSeconds} seconds. ";
-                         //    }
-                         //}
-
-                         //if (!String.IsNullOrWhiteSpace(PassPhrase))
-                         //{
-                         //    if (DateTime.Now.Subtract(passSetTime) > TimeSpan.FromSeconds(30))
-                         //    {
-                         //        PassPhrase = String.Empty;
-                         //    }
-                         //    else
-                         //    {
-                         //        statusMessage += $"Passphrase cleared in {DateTime.Now.Subtract(passSetTime).TotalSeconds} seconds. ";
-                         //    }
-                         //}
-
-                         if (!String.IsNullOrWhiteSpace(statusMessage))
-                         {
-                             //statusStrip1.Invoke((Action)delegate () { statusStrip1.Text = statusMessage; });
-                         }
-                     }
-                    , null, 1000, 1000);
+                    // Memory walker timer, which we'll ideally want to use to annoy any potential
+                    // binaries that are sniffing the users memory. Ideally, this will also mess with hid
+                    // messages in the future (via admin)
+                    //WriteToLogFile($"Timer started", "Startup");
+                    //passphraseClearTimer = new System.Threading.Timer(
+                    // _ =>
+                    // {
+                         
+                    // }
+                    //, null, 1000, 1000);
 
                 }
                 catch (Exception ex)
@@ -112,7 +76,7 @@ namespace BChipDesktop
                 }
             }
         }
-
+        
         private void _monitor_CardInserted(object sender, CardStatusEventArgs e)
         {
             if (e.State == SCRState.Present)
@@ -132,61 +96,6 @@ namespace BChipDesktop
                     {
                         ScanAndLoadConnectedCards(insertedCard);
                     }
-                    /*
-if (insertedCard.Type() == CardType.BChip)
-{
-using (var ctx = _contextFactory.Establish(SCardScope.System))
-{
-    using (var isoReader = new IsoReader(ctx, insertedCard.ReaderName, SCardShareMode.Shared, SCardProtocol.Any))
-    {
-        Response response = isoReader.Transmit(AdpuHelper.RetrieveFullCardData());
-        if (response.HasData)
-        {
-            byte[] data = response.GetData();
-            byte[] mlvi = data.Skip(0x20).Take(BChipMemoryLayout_BCHIP.MLVI_MAX_DATA).ToArray();
-            byte[] carddata = data.Skip(0x28).ToArray();
-
-            // At this stage, the card has not yet been validated/parsed. Another thread should handle cleanup/de-dupes
-            insertedCard.SmartCardData = new BChipMemoryLayout_BCHIP(mlvi, carddata, true, PKStatus.NotValidated);
-
-            bool foundCard = false;
-            if (!LoadedBChips.IsEmpty)
-            {
-                foreach (BChipSmartCard bchip in LoadedBChips)
-                {
-                    if (bchip.ATR.SequenceEqual(e.Atr))
-                    {
-                        foundCard = true;
-                        bchip.IsConnected = true;
-                        bchip.SmartCardData =
-                            new BChipMemoryLayout_BCHIP(mlvi, carddata, true, PKStatus.NotValidated);
-                    }
-                }
-            }
-            if (!foundCard)
-            {
-                LoadedBChips.Add(insertedCard);
-            }
-
-            InsertCardGrid.Dispatcher.BeginInvoke(new Action(() =>
-            {
-                InsertCardGrid.Visibility = Visibility.Collapsed;
-            }));
-
-            // Show the card page
-
-
-            // TODO: show inserted card
-            //MessageBox.Show("Card inserted. Need to actually show the card page dialog.");
-        }
-        else
-        {
-            WriteToLogFile("Card added had no data to download", "_monitor_CardInserted");
-        }
-    }
-}
-}
-*/
                 }
                 catch (Exception ex)
                 {
@@ -198,18 +107,16 @@ using (var ctx = _contextFactory.Establish(SCardScope.System))
 
         private void _monitor_CardRemoved(object sender, CardStatusEventArgs e)
         {
-            if (!LoadedBChips.IsEmpty)
-            { 
-                foreach (BChipSmartCard bchip in LoadedBChips)
+            if (LoadedBChips != null)
+            {
+                if (LoadedBChips.ReaderName == e.ReaderName)
                 {
-                    if (bchip.ATR.SequenceEqual(e.Atr))
-                    {
-                        bchip.IsConnected = false;
-                    }
+                    ChangePageUi(PageToShow.NoCard, null);
+                    // single card only.
+                    // bchip.IsConnected = false;
+                    LoadedBChips = null;
                 }
             }
-
-            ChangePageUi(PageToShow.NoCard, null);
         }
 
         public enum PageToShow
@@ -294,16 +201,26 @@ using (var ctx = _contextFactory.Establish(SCardScope.System))
 
             return PageToShow.Error;
         }
-        
-        private void StartMonitoring()
+
+        private void StopMonitoring()
         {
-            if (_monitor.Monitoring)
+            if (_monitor != null && _monitor.Monitoring)
             {
                 _monitor.Cancel();
+                MonitorFactory.Instance.Release(_monitor);
             }
-            
+        }
+
+        private void StartMonitoring()
+        {
             string[] res = GetReaderNames();
-            _monitor.Start(res);
+            if (res.Length > 0)
+            {
+                _monitor = MonitorFactory.Instance.Create(SCardScope.System);
+                _monitor.CardInserted += _monitor_CardInserted;
+                _monitor.CardRemoved += _monitor_CardRemoved;
+                _monitor.Start(res);
+            }
         }
         
         private void ScanAndLoadConnectedCards(BChipSmartCard insertedCard = null)
@@ -366,7 +283,7 @@ using (var ctx = _contextFactory.Establish(SCardScope.System))
 
                                 // At this stage, the card has not yet been validated/parsed. Another thread should handle cleanup/de-dupes
                                 insertedCard.SmartCardData = new BChipMemoryLayout_BCHIP(mlvi, carddata, true, PKStatus.NotValidated);
-                                LoadedBChips.Add(insertedCard);
+                                LoadedBChips = insertedCard;
 
                                 ChangePageUi(PageToShow.Ready, insertedCard);
                             }
@@ -409,11 +326,99 @@ using (var ctx = _contextFactory.Establish(SCardScope.System))
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             WriteToLogFile($"App closing", "Main");
+        }
 
-            if (_monitor != null)
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // Hook into the message pump
+            HwndSource source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            if (source != null)
             {
-                _monitor.Cancel();
-                _monitor.Dispose();
+                source.AddHook(HwndHandler);
+                DeviceEventMonitor.RegisterDeviceNotification(source.Handle);
+            }
+        }
+
+        // Message pump hook
+        private IntPtr HwndHandler(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+        {
+            if (msg == DeviceEventMonitor.WmDevicechange)
+            {
+                switch ((int)wparam)
+                {
+                    case DeviceEventMonitor.DbtDeviceRemoveComplete:
+                        // Not too many devices leaving on a normal basis, just do a sanity scan each removal
+                        CheckIfReaderRemoved();
+                        break;
+                    case DeviceEventMonitor.DbtDeviceArrival:
+                        DeviceEventMonitor.DEV_BROADCAST_HDR hdr;
+                        hdr = (DeviceEventMonitor.DEV_BROADCAST_HDR)Marshal.PtrToStructure(lparam, typeof(DeviceEventMonitor.DEV_BROADCAST_HDR));
+                        if (hdr.dbch_devicetype == DeviceEventMonitor.DbyDevTypDeviceInterface) 
+                        {
+                            DeviceEventMonitor.DEV_BROADCAST_DEVICEINTERFACE deviceInterface = (DeviceEventMonitor.DEV_BROADCAST_DEVICEINTERFACE)Marshal.PtrToStructure(lparam, typeof(DeviceEventMonitor.DEV_BROADCAST_DEVICEINTERFACE));
+                            string usbDeviceIdentifier = new string(deviceInterface.dbcc_name);
+                            // If we find a "slot", we'll start scanning for cards
+                            CheckIfReaderConnected();
+                        }
+                        break;
+                }
+            }
+
+            handled = false;
+            return IntPtr.Zero;
+        }
+
+        private static Object readLock = new object();
+        private void CheckIfReaderRemoved()
+        {
+            if (LoadedBChips != null)
+            {
+                if (Monitor.TryEnter(readLock))
+                {
+                    try
+                    {
+                        string readerName = LoadedBChips.ReaderName;
+
+                        string[] readers = GetReaderNames();
+                        foreach (string reader in readers)
+                        {
+                            if (readerName == reader)
+                            {
+                                return;
+                            }
+                        }
+
+                        // Card pulled with reader
+                        LoadedBChips = null;
+                        ChangePageUi(PageToShow.NoCard, null);
+                        StopMonitoring();
+                    }
+                    finally
+                    {
+                        Monitor.Exit(readLock);
+                    }
+                }
+            }
+        }
+
+        private void CheckIfReaderConnected()
+        {
+            if (LoadedBChips == null)
+            {
+                if (Monitor.TryEnter(readLock))
+                {
+                    try
+                    {
+                        ScanAndLoadConnectedCards();
+                        StartMonitoring();
+                    }
+                    finally
+                    {
+                        Monitor.Exit(readLock);
+                    }
+                }
             }
         }
 
@@ -460,5 +465,6 @@ using (var ctx = _contextFactory.Establish(SCardScope.System))
         {
 
         }
+
     }
 }
